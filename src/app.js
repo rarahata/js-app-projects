3a7db7eeab0909b9ae12f835a44a4e574564c38c
rarahata <rei.arahata@gmail.com>
2021-10-08 13:51:54 +0100

    add display weather function

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-function displayTempandCity(response){
+function displayWeather(response){
   let temp = document.querySelector("#temp");
   temp.innerHTML = Math.round(response.data.main.temp);
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let windSpeed = document.querySelector("#wind");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  let icon = document.querySelector("#weather-icon");
+  icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+} 
+
+function getCity(city){
+  let apiKey = "7796ed76d4738ed90e39d5875eb78f75";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
-function getCity(event){
+function searchCity(event){
   event.preventDefault();
   let cityInput = document.querySelector("#city-Input");
-  let apiKey = "7796ed76d4738ed90e39d5875eb78f75";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTempandCity);
+  getCity(cityInput.value);
 }
 
 let search = document.querySelector("#search-form");
-search.addEventListener("submit", getCity);
+search.addEventListener("submit", searchCity);
+
+getCity("London");
