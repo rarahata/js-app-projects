fc7854188b1aaf62d964cb21df0d19b774d0bc07
rarahata <rei.arahata@gmail.com>
2021-10-08 13:13:33 +0100

    add display temp and city function

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-function displayCity(event){
+function displayTempandCity(response){
+  let temp = document.querySelector("#temp");
+  temp.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+}
+
+function getCity(event){
   event.preventDefault();
   let cityInput = document.querySelector("#city-Input");
-  let city = document.querySelector("#city");
-  city.innerHTML =  cityInput.value;
+  let apiKey = "7796ed76d4738ed90e39d5875eb78f75";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTempandCity);
 }
 
 let search = document.querySelector("#search-form");
-search.addEventListener("submit", displayCity);
+search.addEventListener("submit", getCity);
